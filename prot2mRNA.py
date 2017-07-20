644f58b9eb136bd46808e94561eb20a78ae746a4
rferrucci <ronald.ferrucci@gmail.com>
2017-07-20 11:21:55 -0500

    Update prot2mRNA.py

--- prot2mRNA.py
+++ prot2mRNA.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
+from rosalind import *
 
-#get number of mRNA sequences possible from protein sequence, modelo 1,000,000
-
-def prot2mrna(seq):
-	f = open('./geneticcode.dat','r').readlines()
-	revCode = {}
-	N = 1
-	for i in f:
-		x = i.strip().split()
-		if x[1] in revCode:
-			revCode[x[1]] += 1
-		else: revCode[x[1]] = 1
-		
-   
-	#get number of possible sequences
-	for i in range(len(seq)):
-		N *= revCode[seq[i]]
-	
-	#don't forget stop codon
-	return N % 1000000
-	
+"""Solution to the Inferring mRNA from Protein problem in the Bioinformatics 
+Stronghold section of Rosalind
+location: http://rosalind.info/problems/mrna/
+Problem: return the number of differences between a pair of DNA sequences
+"""
 if __name__ == "__main__":
 
-    seq = open('data/rosalind_mrna.txt').read().strip()
-    print (prot2mrna(seq))
+	seq = open('data/rosalind_mrna.txt').read()
+	seq = sequence(seq.strip())
+	
+	print (seq.prot2mrna())
