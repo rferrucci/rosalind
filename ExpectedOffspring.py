8cb8518403364d72b87c4678d6e6073d5e204d47
rferrucci <ronald.ferrucci@gmail.com>
2017-07-19 22:37:41 -0500

    Add files via upload

---
+++ ExpectedOffspring.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from rosalind import *
+
+"""Solution to the Calculating Expected Offspring problem in the Bioinformatics Stronghold section of Rosalind
+location: http://rosalind.info/problems/iev/
+Problem: given the number of couples possessing certain genotypes, what is the expected number of offspring 
+displaying to dominant phenotype.
+"""
+
+if __name__ == "__main__":
+	p = calculatingExpOffspring()
+	print(p)
