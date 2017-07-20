fe3444eac70a0668b9e8543bbc23d63e026a0f15
rferrucci <ronald.ferrucci@gmail.com>
2017-07-19 21:18:26 -0500

    Update revComplement.py

--- revComplement.py
+++ revComplement.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 from rosalind import *
 
+"""Solution to problem in the Bioinformatics Stronghold section of Rosalind
+location: http://rosalind.info/problems/revc/
+Problem: return the reverse complement of a dna sequence
+"""
+
 # return the reverse complement of a strand of DNA
 if __name__ == "__main__":	
 	f = open("data/rosalind_cmp.txt",'r').readlines()
