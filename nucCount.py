080c9abd2f77b5efe09f6dd5dce4f11d66f4be5e
rferrucci <ronald.ferrucci@gmail.com>
2017-07-19 21:20:02 -0500

    Update nucCount.py

--- nucCount.py
+++ nucCount.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 from rosalind import *
 
-"""Solution to the Counting DNA Nucleotides problem in the Bioinformatics Stronghold section of Rosalind
-location: http://rosalind.info/problems/dna/
-Problem: return count of nucleotides for given DNA sequence
+"""Solution to the Transcribing DNA into RNA problem in the Bioinformatics Stronghold section of Rosalind
+location: http://rosalind.info/problems/rna/
+Problem: transcribe dna sequence to rna
 """
 
 # outputs counts of nucleotides from a sequence
 if __name__ == "__main__":	
-	f = open("data/rosalind_dna.txt",'r').readlines()
+	f = open("data/rosalind_rna.txt",'r').readlines()
 	fasta, seqs = fastaParse(f)
 	seq = sequence(seqs[0])
-	counts = seq.nucleotideCount()
-	print(" ".join([str(c) for c in counts.values()]))
-	
+	rna = seq.convertToRNA()
+	print(rna)
