4c3ebefb1895d77d5cbcd8ad99c72f4fe8f26702
rferrucci <ronald.ferrucci@gmail.com>
2017-07-20 11:26:02 -0500

    Update rnaSplicing.py

--- rnaSplicing.py
+++ rnaSplicing.py
@@ -8,19 +8,13 @@ Problem: find longest common motif shared amongst a set of sequences.
 
 if __name__ == "__main__":
 	f=open('data/rosalind_splc.txt','r').readlines()
-
 	fasta, sequences = fastaParse(f)
-
 	dna = sequences[0].strip()
-
 	introns=[sequences[i].strip() for i in range(1, len(sequences))]
-	
 	seq = sequence(dna)	
 	seq.spliceIntrons(introns)
-
 	dna = sequence(seq.mDNA)
 	dna.convertToRNA()
 	rna = sequence(dna.rna)
-
 	rna.getProteinSequence()
-
+	print(rna.protein)
