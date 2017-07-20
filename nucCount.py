61f8d87570f33f5763514531e4ac7134c0ff89cf
rferrucci <ronald.ferrucci@gmail.com>
2017-07-19 21:03:14 -0500

    Update nucCount.py

--- nucCount.py
+++ nucCount.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 from rosalind import *
 
+"""Solution to problem in the Bioinformatics Stronghold section of Rosalind
+location: http://rosalind.info/problems/dna/
+Problem: return count of nucleotides for given DNA sequence
+"""
+
 # outputs counts of nucleotides from a sequence
 if __name__ == "__main__":	
 	f = open("data/rosalind_dna.txt",'r').readlines()
