fbaa6a99b7676942e0431456ff4ed515db529d1f
rferrucci <ronald.ferrucci@gmail.com>
2017-09-13 23:44:14 -0500

    Update RabbitPairs.py

--- RabbitPairs.py
+++ RabbitPairs.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 from rosalind import *
 
+"""Solution to the Complementing a Strand of DNA problem in the Bioinformatics 
+Stronghold section of Rosalind
+location: http://rosalind.info/problems/revc/
+Problem: return the reverse complement of a dna sequence
+"""
 
+# return the reverse complement of a strand of DNA
 if __name__ == "__main__":
-	print (rabbitPairs(5, 3))
+	f = open('data/rosalind_fib.txt','r').readlines()
+	m, n = map(int, f[0].split())
+	print (rabbitPairs(m, n))
